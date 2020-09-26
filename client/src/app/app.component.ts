b4534b04cfb1935b566904618a295aaf34924dc3
Dario Fernandez <dariofernandez@hotmail.com>
2020-09-26 14:02:58 -0400

    End Section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,20 +8,9 @@ import { IPagination } from './models/pagination';
 
 export class AppComponent implements OnInit {
   title = 'SkiNet2';
-  products: any[];
-
-  constructor(private http: HttpClient ) { }
+  
+  constructor() { }
 
   ngOnInit(): void {
-
-    // make a connection request to the api
-    //  you can check this from browser, inspect element, console
-    this.http.get('https://localhost:44315/api/products?pageSize=3').
-    subscribe((response: IPagination) => {
-      console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
   }
 }
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,113 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { IProduct } from '../shared/models/product';
+import { ShopService } from './shop.service';
+import { IBrand} from '../shared/models/brand';
+import { IType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
+
+// responsible for displaying the list
+//  and to provide pagination, sorting, searching
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+
+  //static property to use for #search tab in shop.component.html
+  @ViewChild('search', {static: false}) searchTerm: ElementRef;
+
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  shopParams = new ShopParams();
+  totalCount: number;
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'}
+  ];
+
+  constructor(private shopService: ShopService) { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    // consume services in Shop service
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+   }
+
+  getProducts() {
+
+    this.shopService.getProducts(this.shopParams)
+    .subscribe(response => {
+
+      this.products = response.data;
+      this.shopParams.pageNumber = response.pageIndex;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
+
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe(response => {
+      // response is an array of brand objects
+      // ... is spread operator -->  means it adds the new object(id,All) to the response array
+      this.brands = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      // response is an array of type objects
+      // ... is spread operator -->  means it adds the new object(id,All) to the response array
+      this.types = [{id: 0, name: 'All'}, ...response];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onBrandSelected(brandId: number) {
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string) {
+    this.shopParams.sort = sort;
+    //this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any) {
+    // to prevent invoking the getProducts twice, check if the page changed
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+      }
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.getProducts();
+  }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+}
